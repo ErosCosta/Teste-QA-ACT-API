a62ac291ad0d7035965fde788014442f6217d975
ErosLCosta <eros.costa@trampay.com>
2025-12-03 12:42:39 -0300

    Fix: habilitando Allure JUnit5

--- src/test/java/com/example/dogapi/BaseTest.java
+++ src/test/java/com/example/dogapi/BaseTest.java
@@ -2,8 +2,12 @@ package com.example.dogapi;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import io.qameta.allure.junit5.AllureJunit5;
 
+@ExtendWith(AllureJunit5.class)
 public class BaseTest {
+
     @BeforeAll
     public static void setup() {
         RestAssured.baseURI = "https://dog.ceo/api/";
